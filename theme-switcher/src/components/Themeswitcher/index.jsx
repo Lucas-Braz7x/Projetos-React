7991597976925ccf973ae34c45f0f8e5643dacc8
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-09-20 21:29:54 -0300

    Cria o componente ThemeSwitcher

---
+++ theme-switcher/src/components/Themeswitcher/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+//import styled from 'styled-components';
+
+const ThemeSwitcher = ({toggleTheme}) => {
+  return <button onClick={toggleTheme}>Alterar tema</button>
+};
+
+export default ThemeSwitcher;
