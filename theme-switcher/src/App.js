0cd6d2321cb29430a7f292a0f61d3f6d36feac63
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-09-20 21:30:43 -0300

    Adiciona a lib styled-components

--- theme-switcher/src/App.js
+++ theme-switcher/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import * as themes from './styles/themes';
+
+import TodoList from './components/TodoList';
+import ThemeSwitcher from './components/ThemeSwitcher';
+import ThemeContext from './styles/themes/context';
+import { useState } from 'react';
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState(themes.dark);
+
+  const toggleTheme = () => {
+    setCurrentTheme(currentTheme === themes.dark ? themes.light : themes.dark);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <ThemeContext.Provider value={currentTheme}>
+      <ThemeSwitcher toggleTheme={toggleTheme}/>
+        <ThemeContext.Consumer>
+          {(theme) => <TodoList theme={theme}/>}
+        </ThemeContext.Consumer>
+      </ThemeContext.Provider>
     </div>
   );
 }
