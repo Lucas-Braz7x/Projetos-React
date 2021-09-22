2d34c0fb769ac0ee9c86aa51d5dad7f5d9023570
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-09-21 21:52:08 -0300

    Add lib react-switch and styled-components

---
+++ theme-ts/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import GlobalStyled from './styles/global';
+
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyled />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
