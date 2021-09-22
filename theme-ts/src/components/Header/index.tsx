5aa2e7c20e255bde2eb70da2810795904851278b
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-09-21 21:49:34 -0300

    Create Header component

---
+++ theme-ts/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Container} from './styles';
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      Nois
+    </Container>
+  );
+}
+
+export default Header;
