9f6f797c7e3ad142430f432698fe775ccfe9c3af
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-09-29 18:04:51 -0300

    Add themeProvider

--- theme-ts/src/App.tsx
+++ theme-ts/src/App.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 
+import usePersistedState from './utils/usePersistedState';
+
 import GlobalStyled from './styles/global';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
 
 import Header from './components/Header';
-
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
 
 function App() {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light); 
+  };
+
   return (
-    <div className="App">
-      <GlobalStyled />
-      <Header />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyled />
+        <Header toggleTheme={toggleTheme}/>
+      </div>
+    </ThemeProvider>
   );
 }
 
